ed7dff481c2ea4af3868090038203a40e88f8a2f
extantproject <extantproject@test.test>
2015-08-22 18:11:17 -0400

    fixed up the jay sawhn data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var apps = {"jonathanpeterwu/mario-threejs": "https://raw.githubusercontent.com/jonathanpeterwu/mario-threejs/master/app.js"}
-
-var whitelist = [ { 'shortname' : 'Mario', 'url' : 'https://exaample/jonathanpeterwu/mario-threejs/master/app.html' }, { 'shortname' : 'Wario', 'url' : 'https://exaample.test' }]
+var whitelist = [
+	{ 'name' : 'Mario', 'uri' : 'http://mario5.florian-rappl.de/' },
+	{ 'name' : 'Computer', 'uri' : 'http://bellard.org/jslinux/' },
+	{ 'name' : 'Doom', 'uri' : 'http://github.int13h.com/webgldoom/doom.html#e1m1' },
+	{ 'name' : 'Commodore64', 'uri' : 'http://terminal.jcubic.pl/commodore64/' },
+	{ 'name' : 'Stock', 'uri' : 'http://rtstock.co/' },
+	{ 'name' : 'WebGL', 'uri' : 'http://www.smartjava.org/ltjs/chapter-04/05-mesh-face-material.html' }
+]
 
 $('#mario').click(function() {
 	window.open('http://mario5.florian-rappl.de/');
